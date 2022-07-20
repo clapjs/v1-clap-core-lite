a5b43594faa1a218d3c55b95fdccace4c2927ca0
xuyongchao <mail_xyc@sina.com>
2022-07-20 14:47:07 +0800

    Initial commit

--- app/model/org_organ.js
+++ app/model/org_organ.js
@@ -1,26 +1,24 @@
 'use strict';
 
 module.exports = app => {
-    const collection = require('path').basename(__filename, '.js');
+  const attributes = {
+    organCode: {
+      name: '组织编码',
+      type: String,
+    },
+    organName: {
+      name: '组织名称',
+      type: String,
+    },
+    license: {
+      name: 'license',
+      type: Date,
+    },
+    order: {
+      name: '排序号',
+      type: Number,
+    },
+  };
 
-    const attributes = {
-        organCode: {
-            name: '组织编码',
-            type: String
-        },
-        organName: {
-            name: '组织名称',
-            type: String
-        },
-        license: {
-            name: 'license',
-            type: Date,
-        },
-        order: {
-            name: '排序号',
-            type: Number
-        },
-    };
-
-    return app.clapMongooseSchema(attributes,true);
+  return app.clapMongooseSchema(attributes, true);
 };
--- app/model/sys_file.js
+++ app/model/sys_file.js
@@ -1,28 +1,28 @@
 'use strict';
 
 module.exports = app => {
-    const attributes = {
-        fileCode: {
-            name: '文件编码',
-            type: String,
-        },
-        fileName: {
-            name: '文件名称',
-            type: String,
-        },
-        fileType: {
-            name: '文件类型',
-            type: String,
-        },
-        fileSize: {
-            name: '文件大小',
-            type: String,
-        },
-        filePath: {
-            name: '文件路径',
-            type: String,
-        },
-    };
+  const attributes = {
+    fileCode: {
+      name: '文件编码',
+      type: String,
+    },
+    fileName: {
+      name: '文件名称',
+      type: String,
+    },
+    fileType: {
+      name: '文件类型',
+      type: String,
+    },
+    fileSize: {
+      name: '文件大小',
+      type: String,
+    },
+    filePath: {
+      name: '文件路径',
+      type: String,
+    },
+  };
 
-    return app.clapMongooseSchema(attributes, false);
+  return app.clapMongooseSchema(attributes, false);
 };
