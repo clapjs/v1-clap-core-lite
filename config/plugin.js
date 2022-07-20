2d2eba9b16311f55708f65e570ab36877bb02175
xuyongchao <mail_xyc@sina.com>
2022-07-20 14:41:59 +0800

    Initial commit

--- config/plugin.js
+++ config/plugin.js
@@ -2,28 +2,27 @@
 
 // add you build-in plugin here, example:
 module.exports = {
-    static: {
-        enable: true,
-    },
-    security: {
-        enable: true,
-    },
-    cors: {
-        enable: true,
-        package: 'egg-cors',
-    },
-    nunjucks: {
-        enable: true,
-        package: 'egg-view-nunjucks',
-    },
-    io: {
-        enable: true,
-        package: 'egg-socket.io',
-    },
-    clapMongoose: {
-        enable: true,
-        package: 'egg-clap-mongoose',
-    },
+  static: {
+    enable: true,
+  },
+  security: {
+    enable: true,
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  nunjucks: {
+    enable: true,
+    package: 'egg-view-nunjucks',
+  },
+  io: {
+    enable: true,
+    package: 'egg-socket.io',
+  },
+  mongoose: {
+    enable: true,
+    package: '@clapjs/egg-mongoose',
+  },
 };
-
 
